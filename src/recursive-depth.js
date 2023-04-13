dd0615c6bdf12ee941ca221ff1c59991c63e41a5
Nikita Naumov <naumovn808@gmail.com>
2023-04-13 22:21:14 +0500

    feat: task matrix-elements-sum is done

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
+
+  calculateDepth() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
+
+
 
 module.exports = {
   DepthCalculator
