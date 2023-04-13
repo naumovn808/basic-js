a7162e4822664ebf0139a781668675fb6bfc9a4e
Nikita Naumov <naumovn808@gmail.com>
2023-04-13 13:20:02 +0500

    feat: task carbon-dating is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  
+  if (!sampleActivity || sampleActivity <= 0 || typeof sampleActivity != 'string') return false;
+
+  const result = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+
+  return result > 0 ? result : false;
 }
 
 module.exports = {
